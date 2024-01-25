46f86336400731320a7938b078674f80b4db03fa
Raphael Simon <114670310+RaphaelSimonCNAM@users.noreply.github.com>
2024-01-25 11:32:43 +0100

    Update Code_raph.py

--- Code_raph.py
+++ Code_raph.py
@@ -1,11 +1,30 @@
-import numpy as np              # linear algebra
-import pandas as pd             # data processing, CSV file I/O (e.g. pd.read_csv)
-import os                       # files handling
-import matplotlib.pyplot as plt # plotting & dataviz
+import numpy as np             
+import pandas as pd             
+import os                       
+import matplotlib.pyplot as plt 
 from IPython.display import display
 from random import seed
 
-filepath ='C:/Users/rapha/OneDrive/Bureau/fouillesEtEntreposage/projet/financement.csv'
-financement = pd.read_csv(filepath,sep=',', header=0,index_col=0)
+filepath = os.path.join(os.path.dirname(__file__), 'financement.csv')
+financement = pd.read_csv(filepath, sep=',', header=0, index_col=0)
 
-print(financement.head(20))
+# print(financement.head(20))
+
+#fonction detail dataframe
+def resume_dataframe(df):
+    resume_df = pd.DataFrame(index=df.columns)
+    resume_df['Type'] = df.dtypes.values
+    resume_df['Missing'] = df.isnull().sum().values
+
+    resume_df['Unique'] = df.nunique().values
+
+    for col in df.columns:
+        resume_df[f'{col}_1'] = df[col].iloc[0]
+        resume_df[f'{col}_2'] = df[col].iloc[1]
+        resume_df[f'{col}_3'] = df[col].iloc[2]
+
+    return resume_df
+
+resume_transactions = resume_dataframe(financement)
+
+# print(resume_transactions)
